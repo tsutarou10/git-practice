f1e1c8b9c83e4dee63e934477d4efa22ffe6ca21
Tatsuro Miyazaki <tatsumiya05@gmail.com>
2019-05-07 23:22:23 +0900

    [Update]:標準入力でkanagawaを受け取ったらhello kanagawaと出力

--- print_hallo_world.py
+++ print_hallo_world.py
@@ -1,1 +1,8 @@
-print 'hello world'
+def print_hello(prefecture) :
+    if prefecture.lower() == 'kanagawa':
+        print 'hello kanagawa'
+    else :
+        print 'hello world'
+
+prefecture = raw_input()
+print_hello(prefecture)
